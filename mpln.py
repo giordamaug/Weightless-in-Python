7c96d504156aaa1596c3538356f8276508369412
Maurizio Giordano <Maurizio Giordano>
2020-04-23 19:19:18 +0200

    updated

--- mpln.py
+++ mpln.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 #
 # Probabilistic Logic Node - MPLN
 # in Meyers PhD Thesis 1990 (page. 43) 
@@ -15,26 +14,25 @@ import numpy as np
 import argparse
 import sys, os
 import random
-import time
 from PIL import Image
+from utilities import *
+import time
+
 from scipy import misc
-import colored
 import pickle
 import copy
 
 # import scikit-learn
-from sklearn.datasets import load_svmlight_file
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.utils import shuffle
-import scipy.sparse as sps
 
 parser = argparse.ArgumentParser(description='mpln (aleksander training algorithm)')
 parser.add_argument('-D', "--debuglvl", metavar='<debuglevel>', type=int, default=0, help='debug level', required=False)
-parser.add_argument('-i', "--inputfile", metavar='<inputfile>', type=str, help='input file ', required=False)
+parser.add_argument('-i', "--inputfile", metavar='<inputfile>', type=str, help='input file ', required=True)
 parser.add_argument('-d', "--dumpfile", metavar='<dumpfile>', type=str, help='dump file ', required=False)
 parser.add_argument('-C', "--code", metavar='<code>', type=str, default='t', help='coding', required=False, choices=['g', 't','c'])
 parser.add_argument('-z', "--tics", metavar='<ticsno>', type=int, default=4, help='tic number', required=False)
@@ -49,190 +47,6 @@ parser.add_argument('-c', "--cv", help='cv flag', default=False, action='store_t
 mypowers = 2**np.arange(32, dtype = np.uint32)[::]
 tm_progress_ = 0.01
 tm_starttm_ = time.time()
-
-white_color = colored.fg('#ffffff') + colored.bg('#ffffff')
-black_color = colored.fg('#000000') + colored.bg('#000000')
-
-def path_leaf(path):
-    head, tail = os.path.split(path)
-    return tail or os.path.basename(head)
-
-def read_pics_dataset(rootdir, labels=[0,1]):
-    dirs = next(os.walk(rootdir))[1]
-    X = np.empty([0,1024],dtype=int)
-    y = np.empty([0],dtype=int)
-    for dir in dirs:
-        if int(dir) in labels:
-            for f in os.listdir(os.path.join(rootdir,dir)):
-                if not f.startswith('.'):
-                    filename = os.path.join(rootdir,dir,f)
-                    if os.path.isfile(filename):
-                        #img = Image.open(filename).convert('L')
-                        #np_img = np.array(img)
-                        np_img = misc.imread(filename)
-                        np_img = ~np_img
-                        np_img[np_img > 0] = 1
-                        np_img = np_img.flatten()
-                        X = np.row_stack((X, np_img))
-                        y = np.append(y, [int(dir)])
-    return X,y
-
-def unison_shuffled_copies(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
-
-def read_libsvm_dataset(datapath, size, code='t', scale=True):
-    if datapath.endswith('.libsvm'):
-        X, y = load_svmlight_file(datapath)
-        if sps.issparse(X):
-            X = X.toarray()
-    else:
-        raise Exception('Unsupported file format!')
-    # dataset normalization (scaling to 0-1)
-    if scale:
-        scaler = MinMaxScaler(feature_range=(0.0, 1.0))
-        X = scaler.fit_transform(X)
-        X = X.astype(np.float)
-        y = y.astype(np.float)
-
-    # binarize (histogram)
-    tX = (X * size).astype(np.int32)
-
-    if code == 'g':
-        ticsize = size.bit_length()
-        nX = np.zeros([tX.shape[0],tX.shape[1]*ticsize], dtype=np.int)
-        graycode = genCode(ticsize)
-        for r in range(tX.shape[0]):
-            newRow = [int(e) for e in list(''.join([graycode[tX[r,i]] for i in range(tX.shape[1])]))]
-            for i in range(tX.shape[1]*ticsize):
-                 nX[r,i] = newRow[i]
-    elif code == 't':
-        nX = np.zeros([tX.shape[0],tX.shape[1]*size], dtype=np.int)
-        for r in range(tX.shape[0]):
-            for i in range(tX.shape[1]*size):
-                if i % size < tX[r,int(i / size)]:
-                    nX[r,i] = 1
-    elif code == 'c':
-        nX = np.zeros([tX.shape[0],tX.shape[1]*size], dtype=np.int)
-        for r in range(tX.shape[0]):
-            for i in range(tX.shape[1]*size):
-                if i % size + 1== tX[r,int(i / size)]:
-                    nX[r,i] = 1
-    else:
-        raise Exception('Unsupported data code!')
-    return nX, y
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[0;32m'
-    WHITEBLACK = '\033[1m\033[40;37m'
-    BLUEBLACK = '\033[1m\033[40;94m'
-    YELLOWBLACK = '\033[1m\033[40;93m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-
-def timing_init():
-    global tm_starttm_
-    global tm_progress_
-    tm_progress_ = 0.01
-    tm_starttm_ = time.time()
-    
-def timing_update(i,match,clr=color.BLUE,title='train',size=100, lasterr=None,error=None):
-    global tm_starttm_
-    global tm_progress_
-    tm,tme = compTime(time.time()-tm_starttm_,tm_progress_)
-    tm_progress_ = printProgressBar(title, tm, tme, clr, color.RED, i+1,match,tm_progress_,size, lasterr, error)
-
-def printProgressBar(title,time,etime,basecolor, cursorcolor, linecnt,match,progress,size, lasterr, error):
-    barwidth = 70
-    progress = linecnt / float(size);
-    str = '%s |' % title
-    pos = int(barwidth * progress)
-    str += basecolor
-    for p in range(barwidth):
-        if p < pos:
-            str += u'\u2588'
-        elif p == pos:
-            str += color.END + cursorcolor + u'\u2588' + color.END + basecolor
-        else:
-            str += u'\u2591'
-    str += color.END + '| ' + "{:>3}".format(int(progress * 100.0)) + ' % ' + color.YELLOWBLACK + ' ' + etime + ' ' + color.WHITEBLACK + time + ' ' + color.END
-    if lasterr is not None: 
-        str += ' LastErr {:.2f} '.format(lasterr)
-    if error is not None: 
-        str += ' Err {:.2f} '.format(error)
-    if match:
-        str += color.GREEN + u'\U0001F604' + color.END
-    else:
-        str += color.RED + u'\U0001F625' + color.END
-    sys.stdout.write("\r%s" % str)
-    sys.stdout.flush()
-    return progress
-
-def compTime(deltatime,progress):
-    hours, rem = divmod(deltatime*((1.0-progress) / progress), 3600)
-    hourse, reme = divmod(deltatime, 3600)
-    minutes, seconds = divmod(rem, 60)
-    minutese, secondse = divmod(reme, 60)
-    tm = "{:0>2}:{:0>2}:{:02.0f}".format(int(hours),int(minutes),seconds)
-    tme = "{:0>2}:{:0>2}:{:02.0f}".format(int(hourse),int(minutese),secondse)
-    return tm,tme
-
-def print_confmatrix(table,fieldsize=3,decimals=3):
-    nclasses = len(table)
-    hfrmt = '{0: >%d}' % fieldsize
-    dfrmt = '%%%dd' % fieldsize
-    ffrmt = '%%%d.0%df' % (fieldsize,decimals)
-    str = ('\n ' * fieldsize)
-    for c in range(nclasses):
-        str +=  ' '  + color.BOLD + hfrmt.format(c) + color.END
-    print(str)
-    print((' ' * fieldsize) + '┌' + ('─' * fieldsize + '┬') * (nclasses-1) + ('─' * fieldsize) + '┐')
-    for k in range(nclasses):
-        str = color.BOLD + hfrmt.format(k) + color.END
-        for j in range(nclasses):
-            if table[k][j]==0:
-                str += '│' + (' '* fieldsize)
-                continue
-            if j==k:
-                str += '│' + dfrmt % (table[k][j])
-            else:
-                str += '│' + color.RED + dfrmt % (table[k][j]) + color.END
-        str += '│'
-        print(str + '')
-    print((' ' * fieldsize) + '└' + ('─' * fieldsize + '┴') * (nclasses-1) + ('─' * fieldsize) + '┘')
-
-def print_data(r,size,skip=' '):
-    rep = '\n'
-    for i,e in enumerate(r):
-        if e == 0: 
-            rep += black_color + '%s'%(skip) + colored.attr('reset')
-        else:
-            rep += white_color + '%s'%(skip) + colored.attr('reset')
-        if (i+1) % size == 0:
-            rep += '\n'
-    print(rep)
-
-def genCode(n):
-    if n == 0:
-        return ['']
-    
-    code1 = genCode(n-1)
-    code2 = []
-    for codeWord in code1:
-        code2 = [codeWord] + code2
-        
-    for i in range(len(code1)):
-        code1[i] += '0'
-    for i in range(len(code2)):
-        code2[i] += '1'
-    return code1 + code2   
 
 class PyramMPLN:
     """MPLN Pyramid Classifier (Myers,Aleksander training algorithm).
@@ -291,7 +105,7 @@ class PyramMPLN:
         self._kappa = kappa
         self._novals = omega
         self._values = np.arange(0,omega)
-        self._colors = [colored.fg('#%02x%02x%02x'%(i*255/(omega-1),i*255/(omega-1),i*255/(omega-1))) + colored.bg('#%02x%02x%02x'%(i*255/(omega-1),i*255/(omega-1),i*255/(omega-1))) for i in range(omega)]
+        self._colors = [colored.fg('#%02x%02x%02x'%(int(i*255/(omega-1)),int(i*255/(omega-1)),int(i*255/(omega-1)))) + colored.bg('#%02x%02x%02x'%(int(i*255/(omega-1)),int(i*255/(omega-1)),int(i*255/(omega-1)))) for i in range(omega)]
         # eq. 2.16 (Thesis Myers)
         probafunc = np.vectorize(lambda i: 1 / (1 + np.exp(alpha * ( -2 * ( i /float(omega - 1)) + 1 ))))
         # eq. 2.17 (Thesis Myers)
@@ -447,41 +261,20 @@ def main(argv):
     size = args.tics
 
     # load dataset
-    if args.inputfile:
-        if os.path.isdir(args.inputfile):
-            X, y = read_pics_dataset(args.inputfile,labels=[0,1])
-            #X, y = read_pics_dataset(args.inputfile,labels=[0,1,2,3,4,5,6,7,8,9])
-            #y[y == 2] = 0
-            #y[y == 3] = 1
-            X, y = shuffle(X, y)
-            size = len(X[0])/32
+    if os.path.isdir(args.inputfile):
+        X, y = read_pics_dataset(args.inputfile,labels=[0,1])
+        #X, y = read_pics_dataset(args.inputfile,labels=[0,1,2,3,4,5,6,7,8,9])
+        #y[y == 2] = 0
+        #y[y == 3] = 1
+        X, y = shuffle(X, y)
+        size = len(X[0])/32
+    else:
+        if not os.path.isfile(args.inputfile):
+            raise ValueError("Cannot open file %s" % args.inputfile)
         else:
-            if not os.path.isfile(args.inputfile):
-                raise ValueError("Cannot open file %s" % args.inputfile)
-            if args.inputfile.endswith('.libsvm'):
-                X, y = read_libsvm_dataset(args.inputfile, size, args.code)
-                y[y == -1] = 0
-            else:
-                raise Exception('Unsupported file format!')
-    else:     # try dataset (try: mpln.py -n 4 -w 5 -D 1 -a 2.5)
-        X = np.array([[1, 1, 1, 0, 0],
-                      [0, 1, 1, 1, 0],
-                      [0, 0, 1, 1, 1],
-                      [1, 1, 1, 1, 0],
-                      [0, 1, 1, 1, 1],
-                      [1, 1, 1, 1, 1],
-                      [1, 1, 0, 0, 0],
-                      [0, 0, 0, 1, 1],
-                      [1, 0, 0, 0, 0],
-                      [0, 1, 0, 0, 0],
-                      [0, 0, 1, 0, 0],
-                      [0, 0, 0, 1, 0],
-                      [0, 0, 0, 0, 1],
-                      [0, 0, 1, 1, 0],
-                      [0, 1, 1, 0, 0],
-                      [0, 0, 0, 0, 0]], np.int32)
-
-        y = np.array([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0])
+            X, y = read_dataset_fromfile(args.inputfile)
+            X = binarize(X, size, args.code)
+            y[y == -1] = 0
     class_names = np.unique(y)
     y = y.astype(np.int32)
         
