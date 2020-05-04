6039101006aa1b625d1fb75beb6a96aae8c8a52b
Maurizio Giordano <Maurizio Giordano>
2020-05-04 15:42:04 +0200

    update

--- gsn.py
+++ gsn.py
@@ -382,7 +382,7 @@ class PyramGSN:
         delta = 0
         for i,sample in enumerate(X):
             res = self.test(sample)
-            delta += abs(y[i] - res)
+            delta += abs(y[i] - res) if res != undef else 1
             y_pred = np.append(y_pred,[res]) if res != undef else 1
             if self._dblvl > 0: timing_update(i,True,title='test  ',clr=color.GREEN,size=len(X),error=delta/float(i+1))
         if self._dblvl > 0: print()
