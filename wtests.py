6045cc9b7fa29eae73f2e1d5e82e511824728efc
Maurizio Giordano <maurizio.giordano@cnr.it>
2020-06-15 15:31:48 +0200

    update

--- wtests.py
+++ wtests.py
@@ -28,17 +28,16 @@ from mpln import *
 # try:  https://raw.githubusercontent.com/giordamaug/WiSARD4WEKA/master/datasets/ionosphere.arff
 
 parser = argparse.ArgumentParser(description='weightless models')
-parser.add_argument('-i', "--inputfile", metavar='<inputfile>', type=str, help='input file ', required=True)
-parser.add_argument('-D', "--debuglvl", metavar='<debuglevel>', type=int, default=0, help='debug level', required=False)
-parser.add_argument('-n', "--bits", metavar='<bitsno>', type=int, default=2, help='bit number', required=False)
-parser.add_argument('-z', "--tics", metavar='<ticsno>', type=int, default=10, help='tic number', required=False)
-parser.add_argument('-m', "--map", metavar='<mapseed>', type=int, default=-1,help='mapping seed', required=False)
-parser.add_argument('-t', "--trainmode", metavar='<train mode>', type=str, default='normal', help='learning mode', required=False, choices=['normal', 'lazy','progressive'])
-parser.add_argument('-p', "--policy", metavar='<policy>', type=str, default='d', help='policy', required=False, choices=['c', 'd'])
-parser.add_argument('-M', "--methods", metavar='<methods>', type=str, choices=['WiSARD', 'PyramGSN', 'PyramMPLN', 'SVC', 'RF'], default='PyramGSN',help='method list', required=False,  nargs='+')
-parser.add_argument('-C', "--code", metavar='<code>', type=str, default='t', help='coding', required=False, choices=['g', 't','c'])
-parser.add_argument('-S', "--scale", default=True, action='store_true')
-parser.add_argument('-c', "--cv", help='cv flag', default=False, action='store_true', required=False)
+parser.add_argument('-i', "--inputfile", metavar='<inputfile|dir>', type=str, help='input train dataset file (str) - allowed file format: libsvm, arff - or directory of png images (see OptDigits)', required=True)
+parser.add_argument('-D', "--debuglvl", metavar='<debuglevel>', type=int, default=0, help='debug level (int) - range >= 0 (default 0 no debug)', required=False)
+parser.add_argument('-n', "--bits", metavar='<bitsno>', type=int, default=2, help='bit number (int) - range: 2-32 (default 2)', required=False)
+parser.add_argument('-z', "--tics", metavar='<ticsno>', type=int, default=10, help='tic number (int) - range > 1 (default 10)', required=False)
+parser.add_argument('-m', "--map", metavar='<mapseed>', type=int, default=-1,help='mapping seed (int) -  < 0 for linear mapping (default), 0 for rnd mapping with rnd seed, >0 for rnd mapping with fixed seed', required=False)
+parser.add_argument('-t', "--trainmode", metavar='<train mode>', type=str, default='progressive', help='learning mode (str) - allowed values: "normal", "lazy", "progressive" (default)', required=False, choices=['normal', 'lazy','progressive'])
+parser.add_argument('-p', "--policy", metavar='<policy>', type=str, default='d', help='policy type - allowed values: "d" for deterministic (default), "c" for random choice', required=False, choices=['c', 'd'])
+parser.add_argument('-M', "--method", metavar='<method>', type=str, choices=['WiSARD', 'PyramGSN', 'PyramMPLN', 'SVC', 'RF'], default='WiSARD',help='method list (str list) - allowed values: WiSARD, PyramGSN, PyramMPLN, SVC, RF (default WiSARD)', required=False,  nargs='+')
+parser.add_argument('-C', "--code", metavar='<code>', type=str, default='t', help='data encoding - allowed values: "g" for graycode, "t" for thermometer (default), "c" for cursor', required=False, choices=['g', 't','c'])
+parser.add_argument('-c', "--cv", help='enable flag for 10-fold cross-validation on dataset (default disabled)', default=False, action='store_true', required=False)
 
 def print_measures(method,labels,predictions):
     print_confmatrix(confusion_matrix(labels, predictions))
@@ -100,15 +99,14 @@ def main(argv):
                 y[y == -1] = 0
                 y = y.astype(np.int32)
 
-    print(X.shape,y.shape)
     class_names = np.unique(y)
     dataname = os.path.basename(datafile).split(".")[0]
         
     # train and validate
     if args.cv:
         kf = StratifiedKFold(random_state=0,n_splits=10, shuffle=True)
-        predictions = [np.array([])]* len(args.methods)
-        for i,m in enumerate(args.methods):
+        predictions = [np.array([])]* len(args.method)
+        for i,m in enumerate(args.method):
             ylabels = np.array([])
             for train_index, test_index in kf.split(X,y):
                 nX_train, nX_test = nX[train_index], nX[test_index]
@@ -124,7 +122,7 @@ def main(argv):
     else:
         nX_train, nX_test, y_train, y_test = nX,nX,y,y
         X_train, X_test = X,X
-        for m in args.methods:
+        for m in args.method:
             clf = classifier(m, args.bits, len(nX[0]), class_names, args.debuglvl, args.map, args.trainmode, args.policy)
             if m in ['WiSARD', 'PyramGSN', 'PyramMPLN']: 
                 y_pred = clf.fit(nX_train,y_train).predict_ck(nX_test,y_test)
